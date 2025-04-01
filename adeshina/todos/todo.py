3c4d92da023fb3365f9a186e66eb984f47e73984
siksanov <siksanov@icloud.com>
2025-04-01 18:15:27 +0500

    Параметры пути

--- adeshina/todos/todo.py
+++ adeshina/todos/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from model import Todo
 
 todo_router = APIRouter()
@@ -13,3 +13,15 @@ async def add_todo(todo: Todo) -> dict:
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"tools": todo_list}
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="The \
+ID of the todo to retrieve.")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist"
+    }
