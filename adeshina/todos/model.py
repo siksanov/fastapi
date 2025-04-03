c9b82d796f7e128e7938f1df1a3640e05809120f
siksanov <siksanov@icloud.com>
2025-04-03 12:44:58 +0500

    Создание простого CRUD-приложения

--- adeshina/todos/model.py
+++ adeshina/todos/model.py
@@ -3,3 +3,13 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book."
+            }
+        }
