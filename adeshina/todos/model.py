270c21d40c3f808dfa22ec32d6f8b4638bad7351
siksanov <siksanov@icloud.com>
2025-04-04 14:33:21 +0500

    Использование шаблонов Jinja в FastAPI

--- adeshina/todos/model.py
+++ adeshina/todos/model.py
@@ -1,9 +1,28 @@
+from typing import List, Optional
+
+from fastapi import Form
 from pydantic import BaseModel
-from typing import List
+
 
 class Todo(BaseModel):
-    id: int
+    id: Optional[int] = None
     item: str
+
+    @classmethod
+    def as_form(
+            cls,
+            item: str = Form(...)
+    ):
+        return cls(item=item)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        }
+
 
 class TodoItem(BaseModel):
     item: str
@@ -14,6 +33,7 @@ class TodoItem(BaseModel):
                 "item": "Read the next chapter of the book"
             }
         }
+
 
 class TodoItems(BaseModel):
     todos: List[TodoItem]
