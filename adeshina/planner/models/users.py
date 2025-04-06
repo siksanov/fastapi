5ab5c80cb6eccc0331b3489d54084bca4913657b
siksanov <siksanov@icloud.com>
2025-04-06 19:23:03 +0500

    Реализация моделей

---
+++ adeshina/planner/models/users.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]] = None
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!!!",
+                "events": [],
+            }
+        }
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": "fastapi@packt.com",
+            "password": "strong!!!",
+            "events": [],
+        }
