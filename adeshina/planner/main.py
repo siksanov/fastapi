9e29685fb2dcb606c00100c898e5caa22c85b848
siksanov <siksanov@icloud.com>
2025-04-07 18:55:47 +0500

    Создание базы данных

--- adeshina/planner/main.py
+++ adeshina/planner/main.py
@@ -1,15 +1,30 @@
 from fastapi import FastAPI
+from database.connection import conn
+
 from routes.users import user_router
 from routes.events import event_router
+from contextlib import asynccontextmanager
+
 
 import uvicorn
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn()
+    yield
+
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 # Register routes
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080,
