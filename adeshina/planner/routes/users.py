1b1441a7151e739c0c2825d51611ba35404f64cd
siksanov <siksanov@icloud.com>
2025-04-08 18:36:32 +0500

    Настройка MongoDB

--- adeshina/planner/routes/users.py
+++ adeshina/planner/routes/users.py
@@ -1,37 +1,40 @@
 from fastapi import APIRouter, HTTPException, status
+from database.connection import Database
+
 from models.users import User, UserSignIn
 
 user_router = APIRouter(
-    tags=["User"]
+    tags=["User"],
 )
 
-users = {}
+user_database = Database(User)
 
 @user_router.post("/signup")
-async def sign_new_user(data: User) -> dict:
-    if data.email in users:
+async def sign_user_up(user: User) -> dict:
+    user_exist = await User.find_one(User.email == user.email)
+    if user_exist:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="User with supplied username exists"
+            detail="User with email provided exists already."
         )
-    users[data.email] = data
+    await user_database.save(user)
     return {
-        "message": "User successfully registered!"
+        "message": "User cerated successfully."
     }
 
 @user_router.post("/signin")
 async def sign_user_in(user: UserSignIn) -> dict:
-    if user.email not in users:
+    user_exist = await User.find_one(User.email == user.email)
+    if not user_exist:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="User does not exist"
+            detail="User is unauthorized"
         )
-    if users[user.email].password != user.password:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Wrong credentials passed"
-        )
-    
-    return {
-        "message": "User signed in successfully"
-    }
+    if user_exist.password == user.password:
+        return {
+            "message": "User signed in successfully."
+        }
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid details passed."
+    )
